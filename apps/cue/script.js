65f6d1b9dfee94886c86995f4655755a5d216493
Wayan Aditya <wayanadityaekc@gmail.com>
2026-08-11 23:27:41 +1000

    add images

--- apps/cue/script.js
+++ apps/cue/script.js
@@ -3,7 +3,7 @@
 // -- site config
 // Naikin angka ini tiap kali isi file di folder partials/ diubah,
 // biar browser narik versi baru dan bukan yang nyangkut di cache.
-const PARTIALS_VERSION = 40;
+const PARTIALS_VERSION = 41;
 
 const WHATSAPP_NUMBER = "61401657862";
 
